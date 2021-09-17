3505897bb3b371ed832bc73adef4f9f3e9f8c33e
jonahjtr <86381032+jonahjtr@users.noreply.github.com>
2021-09-17 15:08:30 -0500

    finished most, next is testing

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,27 +1,60 @@
 import React, { useEffect, useState } from "react";
+import axiosWithAuth from "../helpers/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
-import fetchColorService from '../services/fetchColorService';
+import fetchColorService from "../services/fetchColorService";
 
 const BubblePage = () => {
   const [colors, setColors] = useState([]);
   const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    fetchColorService().then((res) => {
+      setColors(res);
+    });
+  }, []);
 
   const toggleEdit = (value) => {
     setEditing(value);
   };
 
   const saveEdit = (editColor) => {
+    axiosWithAuth()
+      .put(`colors/${editColor.id}`, editColor)
+      .then((res) => {
+        console.log(res);
+
+        const index = colors.findIndex((color) => color.id === editColor.id);
+        colors[index] = editColor;
+        setColors([...colors]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const deleteColor = (colorToDelete) => {
+    axiosWithAuth()
+      .delete(`colors/${colorToDelete.id}`)
+      .then((res) => {
+        setColors(colors.filter((color) => color.id !== colorToDelete.id));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <div className="container">
-      <ColorList colors={colors} editing={editing} toggleEdit={toggleEdit} saveEdit={saveEdit} deleteColor={deleteColor}/>
-      <Bubbles colors={colors}/>
+      <ColorList
+        colors={colors}
+        editing={editing}
+        toggleEdit={toggleEdit}
+        saveEdit={saveEdit}
+        deleteColor={deleteColor}
+      />
+      <Bubbles colors={colors} />
     </div>
   );
 };
--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -1,7 +1,14 @@
-import axiosWithAuth from '../helpers/axiosWithAuth';
+import axiosWithAuth from "../helpers/axiosWithAuth";
 
 const fetchColorService = () => {
-    
-}
+  return axiosWithAuth()
+    .get("/colors")
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 export default fetchColorService;
