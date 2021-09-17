2ca197222ced54b68cbddbba1874aa19ebbee63f
jonahjtr <86381032+jonahjtr@users.noreply.github.com>
2021-09-17 15:19:31 -0500

    finished all mvp, tests are buggy even though the app works

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
 
-import { render, screen} from "@testing-library/react";
-import BubblePage from './BubblePage';
+import { render, screen } from "@testing-library/react";
+import BubblePage from "./BubblePage";
+jest.mock("../services/fetchColorService");
 
-test("Renders without errors", ()=> {
-    
+const testColor = {
+  color: "purple",
+  code: { hex: "#9345EE" },
+  id: 1,
+};
+
+test("Renders without errors", () => {
+  render(<BubblePage />);
 });
 
-test("Renders appropriate number of colors passed in through mock", async ()=> {
-    //Keep in mind that our service is called on mount for this component.
+test("Renders appropriate number of colors passed in through mock", async () => {
+  //Keep in mind that our service is called on mount for this component.
+  render(<BubblePage />);
+  const colors = screen.getAllByTestId("color");
+  await waitFor(() => {
+    expect(colors).toHaveLength(1);
+  });
 });
--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
 
-import { render, screen} from "@testing-library/react";
-import ColorList from './ColorList';
+import { render, screen } from "@testing-library/react";
+import ColorList from "./ColorList";
 
 test("Renders an empty list of colors without errors", () => {
+  render(<ColorList color={noColor} />);
 });
 
 test("Renders a list of colors without errors", () => {
+  render(<ColorList color={testColor} />);
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+  const toggleEdit = jest.fn();
+  render(<ColorList color={testColor} />);
+  const editing = screen.queryByTestId("color");
+  userEvent.click(editing);
+  expect(toggleEdit).toBeCalled();
 });
